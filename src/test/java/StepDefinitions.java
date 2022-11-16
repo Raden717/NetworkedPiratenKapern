30db023e956d94e83314917665ab3aafcfd5fd3f
raden <raden.almaden@gmail.com>
2022-11-16 09:48:17 -0500

    CODE: Step definitions to account for all players

--- src/test/java/StepDefinitions.java
+++ src/test/java/StepDefinitions.java
@@ -23,6 +23,34 @@ public class StepDefinitions {
         test.updateScore();
     }
 
+    @Given("Player2 rolls {String[]} dice and Card {String} ")
+    public void forcedRollP2(String[] rolls, String card) {
+        test2 = new Player("Test");
+        test2.roll();
+        test2.setForceDice(rolls);
+        test2.setCard(card);
+    }
+
+    @When("Player2 decides to end turn")
+    public void endTurnP2(){
+        test2.updateAlive();
+        test2.updateScore();
+    }
+
+    @Given("Player3 rolls {String[]} dice and Card {String} ")
+    public void forcedRollP3(String[] rolls, String card) {
+        test3 = new Player("Test");
+        test3.roll();
+        test3.setForceDice(rolls);
+        test3.setCard(card);
+    }
+
+    @When("Player3 decides to end turn")
+    public void endTurnP3(){
+        test3.updateAlive();
+        test3.updateScore();
+    }
+
     @Then("Player1 should have score {int}, Player2 should have score {int}, Player3 should have score {int} ")
     public void endScore(int score1, int score2, int score3){
         assertEquals(score1, test.getScore());
