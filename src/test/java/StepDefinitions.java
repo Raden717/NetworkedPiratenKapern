50df9c1243b2259d3383f86fbed70dc991d8bc64
raden <raden.almaden@gmail.com>
2022-11-16 10:02:02 -0500

    REFACTOR: Step definitions modified to use game procedures to  account for scores and winner properly

--- src/test/java/StepDefinitions.java
+++ src/test/java/StepDefinitions.java
@@ -9,21 +9,28 @@ public class StepDefinitions {
     Player test2;
     Player test3;
 
+    int pointThreshold = 3000;
+
     @Given("Player1 rolls {String[]} dice and Card {String} ")
     public void forcedRollP1(String[] rolls, String card) {
+        pointThreshold = 3000;
         test = new Player("Test");
         test.roll();
         test.setForceDice(rolls);
         test.setCard(card);
     }
 
-    @When("Player1 decides to end turn")
+    @And("Player1 decides to end turn")
     public void endTurnP1(){
         test.updateAlive();
         test.updateScore();
+        test.checkIfWon(pointThreshold);
+        if(test.getScore() > pointThreshold){
+            pointThreshold = test.getScore();
+        }
     }
 
-    @Given("Player2 rolls {String[]} dice and Card {String} ")
+    @And("Player2 rolls {String[]} dice and Card {String} ")
     public void forcedRollP2(String[] rolls, String card) {
         test2 = new Player("Test");
         test2.roll();
@@ -31,13 +38,17 @@ public class StepDefinitions {
         test2.setCard(card);
     }
 
-    @When("Player2 decides to end turn")
+    @And("Player2 decides to end turn")
     public void endTurnP2(){
         test2.updateAlive();
         test2.updateScore();
+        test.checkIfWon(pointThreshold);
+        if(test3.getScore() > pointThreshold){
+            pointThreshold = test3.getScore();
+        }
     }
 
-    @Given("Player3 rolls {String[]} dice and Card {String} ")
+    @And("Player3 rolls {String[]} dice and Card {String} ")
     public void forcedRollP3(String[] rolls, String card) {
         test3 = new Player("Test");
         test3.roll();
@@ -45,10 +56,14 @@ public class StepDefinitions {
         test3.setCard(card);
     }
 
-    @When("Player3 decides to end turn")
+    @And("Player3 decides to end turn")
     public void endTurnP3(){
         test3.updateAlive();
         test3.updateScore();
+        test3.checkIfWon(pointThreshold);
+        if(test3.getScore() > pointThreshold){
+            pointThreshold = test3.getScore();
+        }
     }
 
     @Then("Player1 should have score {int}, Player2 should have score {int}, Player3 should have score {int} ")
@@ -57,6 +72,18 @@ public class StepDefinitions {
         assertEquals(score2, test2.getScore());
         assertEquals(score3, test3.getScore());
     }
+
+    @Then("Player1 should have score {int}, Player2 should have score {int}, Player3 should have score {int} and Player1 should have won {boolean}")
+    public void endScore(int score1, int score2, int score3, boolean won){
+        test.checkIfWon(pointThreshold);
+        assertEquals(score1, test.getScore());
+        assertEquals(score2, test2.getScore());
+        assertEquals(score3, test3.getScore());
+        assertEquals(won, test.getWinner());
+
+    }
+
+
 
 
 
